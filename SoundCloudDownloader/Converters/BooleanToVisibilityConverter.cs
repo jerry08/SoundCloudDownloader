c104c67e3e68d7ece8aea3f61478af6bf974cc29
jerrry08 <jeremyyearwood9@outlook.com>
2023-01-06 14:03:36 -0400

    Cleanup

--- SoundCloudDownloader/Converters/BooleanToVisibilityConverter.cs
+++ SoundCloudDownloader/Converters/BooleanToVisibilityConverter.cs
@@ -7,27 +7,26 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
-namespace SoundCloudDownloader.Converters
-{
-    public class BooleanToVisibilityConverter : IValueConverter
-    {
-        private object GetVisibility(object value)
-        {
-            if (value is not bool)
-            {
-                return Visibility.Collapsed;
-            }
+namespace SoundCloudDownloader.Converters;
 
-            bool objValue = (bool)value;
-            return objValue ? Visibility.Visible : (object)Visibility.Collapsed;
-        }
-        public object Convert(object value, Type targetType, object parameter, CultureInfo language)
+public class BooleanToVisibilityConverter : IValueConverter
+{
+    private object GetVisibility(object value)
+    {
+        if (value is not bool)
         {
-            return GetVisibility(value);
+            return Visibility.Collapsed;
         }
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo language)
-        {
-            throw new NotImplementedException();
-        }
+
+        bool objValue = (bool)value;
+        return objValue ? Visibility.Visible : (object)Visibility.Collapsed;
+    }
+    public object Convert(object value, Type targetType, object parameter, CultureInfo language)
+    {
+        return GetVisibility(value);
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo language)
+    {
+        throw new NotImplementedException();
     }
 }

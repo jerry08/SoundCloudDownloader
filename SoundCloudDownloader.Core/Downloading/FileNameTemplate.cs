3d960120af620229f139efbcc75b595d70dea999
jerrry08 <jeremyyearwood99@gmail.com>
2023-03-12 14:15:25 -0400

    Fix bug #5

--- SoundCloudDownloader.Core/Downloading/FileNameTemplate.cs
+++ SoundCloudDownloader.Core/Downloading/FileNameTemplate.cs
@@ -13,11 +13,11 @@ public class FileNameTemplate
         string? number = null) =>
         PathEx.EscapeFileName(
             template
-                .Replace("$num", number is not null ? $"[{number}]" : "")
+                .Replace("$num", number is not null ? $"{number}" : "")
                 .Replace("$id", $"{track.Id}")
                 .Replace("$title", track.Title)
-                //.Replace("$artist", track.PublisherMetadata.Artist)
-                .Replace("$artist", track.User?.Username)
+                //.Replace("$author", track.PublisherMetadata.Artist)
+                .Replace("$author", track.User?.Username)
                 .Replace("$releasedDate", DateTime.TryParse(track.ReleaseDate?.ToString(), out DateTime releasedDate) ? (releasedDate.ToString("yyyy-MM-dd") ?? "") : "")
                 .Trim() + '.' + ext
         );

fcbf7124443ba080e23af5b067de55bad31b4db2
jerrry08 <jeremyyearwood99@gmail.com>
2023-03-12 20:22:15 -0400

    Add album in filename template

--- SoundCloudDownloader.Core/Downloading/FileNameTemplate.cs
+++ SoundCloudDownloader.Core/Downloading/FileNameTemplate.cs
@@ -16,6 +16,7 @@ public class FileNameTemplate
                 .Replace("$num", number is not null ? $"{number}" : "")
                 .Replace("$id", $"{track.Id}")
                 .Replace("$title", track.Title)
+                .Replace("$album", track.PlaylistName)
                 //.Replace("$author", track.PublisherMetadata.Artist)
                 .Replace("$author", track.User?.Username)
                 .Replace("$releasedDate", DateTime.TryParse(track.ReleaseDate?.ToString(), out DateTime releasedDate) ? (releasedDate.ToString("yyyy-MM-dd") ?? "") : "")

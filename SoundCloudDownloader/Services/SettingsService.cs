de4f5c25cb47af916c6622e866d04b23e5807d9e
jerry08 <jeremyyearwood99@gmail.com>
2024-12-28 13:10:20 -0400

    Update

--- SoundCloudDownloader/Services/SettingsService.cs
+++ SoundCloudDownloader/Services/SettingsService.cs
@@ -8,32 +8,39 @@ using SoundCloudDownloader.Framework;
 namespace SoundCloudDownloader.Services;
 
 [INotifyPropertyChanged]
-public partial class SettingsService()
-    : SettingsBase(
-        Path.Combine(AppContext.BaseDirectory, "Settings.dat"),
-        SerializerContext.Default
-    )
+public partial class SettingsService : SettingsBase
 {
     [ObservableProperty]
     public partial ThemeVariant Theme { get; set; }
 
     [ObservableProperty]
-    public partial bool IsAutoUpdateEnabled { get; set; } = true;
+    public partial bool IsAutoUpdateEnabled { get; set; }
 
     [ObservableProperty]
-    public partial bool ShouldInjectTags { get; set; } = true;
+    public partial bool ShouldInjectTags { get; set; }
 
     [ObservableProperty]
     public partial bool ShouldSkipExistingFiles { get; set; }
 
     [ObservableProperty]
-    public partial string FileNameTemplate { get; set; } = "$title";
+    public partial string FileNameTemplate { get; set; }
 
     [ObservableProperty]
-    public partial int ParallelLimit { get; set; } = 5;
+    public partial int ParallelLimit { get; set; }
 
     [ObservableProperty]
-    public partial string LastContainer { get; set; } = "Mp3";
+    public partial string LastContainer { get; set; }
+
+    public SettingsService()
+        : base(Path.Combine(AppContext.BaseDirectory, "Settings.dat"), SerializerContext.Default)
+    {
+        // Initialize properties here to prevent linux build error CS8050
+        IsAutoUpdateEnabled = true;
+        ShouldInjectTags = true;
+        FileNameTemplate = "$title";
+        ParallelLimit = 5;
+        LastContainer = "Mp3";
+    }
 }
 
 public partial class SettingsService

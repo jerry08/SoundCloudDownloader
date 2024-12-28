40818877ff78d4538d94aa80ca727779337bc31d
jerry08 <jeremyyearwood99@gmail.com>
2024-12-28 11:56:54 -0400

    Use `[ObservableProperty]` now that it supports partial properties

--- SoundCloudDownloader/Services/SettingsService.cs
+++ SoundCloudDownloader/Services/SettingsService.cs
@@ -7,8 +7,6 @@ using SoundCloudDownloader.Framework;
 
 namespace SoundCloudDownloader.Services;
 
-// Can't use [ObservableProperty] here because System.Text.Json's source generator doesn't see
-// the generated properties.
 [INotifyPropertyChanged]
 public partial class SettingsService()
     : SettingsBase(
@@ -16,55 +14,26 @@ public partial class SettingsService()
         SerializerContext.Default
     )
 {
-    private ThemeVariant _theme;
-    public ThemeVariant Theme
-    {
-        get => _theme;
-        set => SetProperty(ref _theme, value);
-    }
+    [ObservableProperty]
+    public partial ThemeVariant Theme { get; set; }
 
-    private bool _isAutoUpdateEnabled = true;
-    public bool IsAutoUpdateEnabled
-    {
-        get => _isAutoUpdateEnabled;
-        set => SetProperty(ref _isAutoUpdateEnabled, value);
-    }
+    [ObservableProperty]
+    public partial bool IsAutoUpdateEnabled { get; set; } = true;
 
-    private bool _shouldInjectTags = true;
-    public bool ShouldInjectTags
-    {
-        get => _shouldInjectTags;
-        set => SetProperty(ref _shouldInjectTags, value);
-    }
+    [ObservableProperty]
+    public partial bool ShouldInjectTags { get; set; } = true;
 
-    private bool _shouldSkipExistingFiles;
-    public bool ShouldSkipExistingFiles
-    {
-        get => _shouldSkipExistingFiles;
-        set => SetProperty(ref _shouldSkipExistingFiles, value);
-    }
+    [ObservableProperty]
+    public partial bool ShouldSkipExistingFiles { get; set; }
 
-    private string _fileNameTemplate = "$title";
-    public string FileNameTemplate
-    {
-        get => _fileNameTemplate;
-        set => SetProperty(ref _fileNameTemplate, value);
-    }
+    [ObservableProperty]
+    public partial string FileNameTemplate { get; set; } = "$title";
 
-    private int _parallelLimit = 5;
-    public int ParallelLimit
-    {
-        get => _parallelLimit;
-        set => SetProperty(ref _parallelLimit, value);
-    }
+    [ObservableProperty]
+    public partial int ParallelLimit { get; set; } = 5;
 
-    private string _lastContainer = "Mp3";
-
-    public string LastContainer
-    {
-        get => _lastContainer;
-        set => SetProperty(ref _lastContainer, value);
-    }
+    [ObservableProperty]
+    public partial string LastContainer { get; set; } = "Mp3";
 }
 
 public partial class SettingsService

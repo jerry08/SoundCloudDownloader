69480ac5f753f0c1e8b305f5861272fe10e1b42b
jerry08 <jeremyyearwood99@gmail.com>
2024-12-28 13:14:53 -0400

    Update

--- SoundCloudDownloader/Services/SettingsService.cs
+++ SoundCloudDownloader/Services/SettingsService.cs
@@ -7,39 +7,63 @@ using SoundCloudDownloader.Framework;
 
 namespace SoundCloudDownloader.Services;
 
+// Can't use [ObservableProperty] here because System.Text.Json's source generator doesn't see
+// the generated properties.
 [INotifyPropertyChanged]
-public partial class SettingsService : SettingsBase
+public partial class SettingsService()
+    : SettingsBase(
+        Path.Combine(AppContext.BaseDirectory, "Settings.dat"),
+        SerializerContext.Default
+    )
 {
-    [ObservableProperty]
-    public partial ThemeVariant Theme { get; set; }
-
-    [ObservableProperty]
-    public partial bool IsAutoUpdateEnabled { get; set; }
-
-    [ObservableProperty]
-    public partial bool ShouldInjectTags { get; set; }
-
-    [ObservableProperty]
-    public partial bool ShouldSkipExistingFiles { get; set; }
-
-    [ObservableProperty]
-    public partial string FileNameTemplate { get; set; }
-
-    [ObservableProperty]
-    public partial int ParallelLimit { get; set; }
-
-    [ObservableProperty]
-    public partial string LastContainer { get; set; }
-
-    public SettingsService()
-        : base(Path.Combine(AppContext.BaseDirectory, "Settings.dat"), SerializerContext.Default)
+    private ThemeVariant _theme;
+    public ThemeVariant Theme
     {
-        // Initialize properties here to prevent linux build error CS8050
-        IsAutoUpdateEnabled = true;
-        ShouldInjectTags = true;
-        FileNameTemplate = "$title";
-        ParallelLimit = 5;
-        LastContainer = "Mp3";
+        get => _theme;
+        set => SetProperty(ref _theme, value);
+    }
+
+    private bool _isAutoUpdateEnabled = true;
+    public bool IsAutoUpdateEnabled
+    {
+        get => _isAutoUpdateEnabled;
+        set => SetProperty(ref _isAutoUpdateEnabled, value);
+    }
+
+    private bool _shouldInjectTags = true;
+    public bool ShouldInjectTags
+    {
+        get => _shouldInjectTags;
+        set => SetProperty(ref _shouldInjectTags, value);
+    }
+
+    private bool _shouldSkipExistingFiles;
+    public bool ShouldSkipExistingFiles
+    {
+        get => _shouldSkipExistingFiles;
+        set => SetProperty(ref _shouldSkipExistingFiles, value);
+    }
+
+    private string _fileNameTemplate = "$title";
+    public string FileNameTemplate
+    {
+        get => _fileNameTemplate;
+        set => SetProperty(ref _fileNameTemplate, value);
+    }
+
+    private int _parallelLimit = 5;
+    public int ParallelLimit
+    {
+        get => _parallelLimit;
+        set => SetProperty(ref _parallelLimit, value);
+    }
+
+    private string _lastContainer = "Mp3";
+
+    public string LastContainer
+    {
+        get => _lastContainer;
+        set => SetProperty(ref _lastContainer, value);
     }
 }
 

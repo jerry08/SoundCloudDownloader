e7aaf43d3f9c46a2b16ad09d798ea1bbe806e261
jerry08 <jeremyyearwood99@gmail.com>
2024-12-28 11:38:35 -0400

    Use .net9

--- SoundCloudDownloader/App.axaml.cs
+++ SoundCloudDownloader/App.axaml.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Net;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -75,9 +74,6 @@ public class App : Application, IDisposable
     public override void Initialize()
     {
         base.Initialize();
-
-        // Increase maximum concurrent connections
-        ServicePointManager.DefaultConnectionLimit = 20;
 
         AvaloniaXamlLoader.Load(this);
     }
